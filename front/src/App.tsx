da26c4d4ae5b84dbd1145196ba33348d104305c3
makoalex <macovei.a2@gmail.com>
2023-11-22 15:11:27 +0100

    [feat] routing with react-router

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import LoginPage from './LoginPage/LoginPage'
+import MapPage from './MapPage/MapPage'
 
 export default function hello() {
   return (
-    <div>hello</div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path ="/map" element={<MapPage />} />
+      </Routes>
+    </Router>
   )
 }
