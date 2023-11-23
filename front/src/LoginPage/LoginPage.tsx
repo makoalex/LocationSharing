0cff69b02b82829b562247bf50a8b7c6f2f02012
makoalex <macovei.a2@gmail.com>
2023-11-23 12:09:12 +0100

    [feat] rendering components and style

--- front/src/LoginPage/LoginPage.tsx
+++ front/src/LoginPage/LoginPage.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import Logo from '../components/Logo'
+import Input from '../components/Input'
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <div className='section flex flex-row justify-center items-center w-full h-screen '>
+      <div className='container relative border-2 border-black border-b-4 flex flex-col justify-center items-center w-3/4 md:w-3/5 lg:w-2/5 h-[350px] bg-primary mx-auto'> 
+      <Logo />
+      <Input/>
+      </div>
+    </div>
   )
 }
