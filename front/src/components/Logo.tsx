25d7f02234bdd06e8a12ab2f18ff3575217aaf95
makoalex <macovei.a2@gmail.com>
2023-11-23 12:08:37 +0100

    [feat] logo component

---
+++ front/src/components/Logo.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Logo() {
+  return (
+    <div>
+        <h3 className=' absolute top-8 left-8 font-primary text-xl font-bold'>SnapMap</h3>
+    </div>
+  )
+}
