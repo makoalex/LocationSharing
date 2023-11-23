98387653c7883a34d1eb41dc87410bd8a8fbddb3
makoalex <macovei.a2@gmail.com>
2023-11-23 12:07:10 +0100

    [style]  added tailwind config

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,10 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      primary: "Lexend Mega",
+      secondary: "Public Sans",
+    },
+    container: {
+      padding: {
+        DEFAULT: "10px",
+      },
+      boxShadow: {
+        xl: "7px 7px 0px -0px #0B2447",
+      },
+    },
+    screens: {
+      xs: "320px",
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1200px",
+    },
+    extend: {
+      colors: {
+        primary: "#ffaa92",
+        callAction: "#a66eff",
+        secondary: "#F5F5F5ff",
+        accent: "#02EAE8ff",
+        nonblue: "#0B2447",
+      },
+    },
   },
   plugins: [],
-}
+};
