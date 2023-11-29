56ba1682b28a6ec3ffe0a46bab940c31480d536a
makoalex <macovei.a2@gmail.com>
2023-11-29 12:29:08 +0100

    [feat] testing the landing page

--- front/cypress/e2e/homePage.cy.js
+++ front/cypress/e2e/homePage.cy.js
@@ -1,5 +1,17 @@
-describe('Home Page', ()=>{
-it('should load home page', ()=>{
-    cy.visit('/')
-})
-})
+const afterLoading = () => cy.visit("/");
+describe("HomePage", () => {
+  beforeEach(() => {
+    afterLoading();
+    cy.wait(1000);
+  });
+  it("should load page", () => afterLoading());
+  it("should have a title", () => {
+    cy.contains('[data-testid="logo"]', "SnapMap").should("exist");
+  });
+  it("should have an input field", () => {
+    cy.get('[data-testid="input"]').should("exist");
+  });
+  it("should have a button", () => {
+    cy.get('[data-testid="button"]').should("exist");
+  });
+});
