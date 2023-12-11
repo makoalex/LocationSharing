113c0e05ddbe769ef92c3e7e794eeec9acd8db35
makoalex <macovei.a2@gmail.com>
2023-12-11 15:36:12 +0100

    [feat] rendering username label on map

--- front/src/MapPage/Marker.tsx
+++ front/src/MapPage/Marker.tsx
@@ -4,10 +4,17 @@ import { markerProps } from "../Types";
 import locationMarker from "../assets/locationMarker.svg";
 
 export default function Marker(props: markerProps) {
-  const {myself, coords, lat,lng, socketId, username } = props;
+  const { myself, coords, lat, lng, socketId, username } = props;
   return (
-    <div className="" >
-      <img src={locationMarker} alt={username} className="w-[35px] h-[50px] hover:translate-y-2 hover:transition-all" />
+    <div className="flex flex-col justify-center items-center">
+      <img
+        src={locationMarker}
+        alt={username}
+        className="w-[35px] h-[50px] hover:translate-y-2 hover:transition-all"
+      />
+      <p className="font-primary text-lg text-accent">
+        {myself ? "me" : username}
+      </p>
     </div>
   );
 }
