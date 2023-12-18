8733d76bb88e77e83eb9845ac8d1a8cbe1db8a37
makoalex <macovei.a2@gmail.com>
2023-12-18 21:38:48 +0100

    [feat] adding unit tests

--- front/src/App.test.tsx
+++ front/src/App.test.tsx
@@ -1,15 +1,47 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import store from './store/store'
-import App from './App';
+import React from "react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+import { PropsWithChildren } from "react";
+import { Provider } from "react-redux";
+import {
+  MemoryRouter,
+  BrowserRouter,
+  Router,
+  BrowserRouterProps,
+} from "react-router-dom";
+import store from "./store/store";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
+jest.mock("./LoginPage/LoginPage", () => ({
+  __esModule: true,
+  default: () => {
+    return <>Mock Login Page</>;
+  },
+}));
+jest.mock("./MapPage/MapPage", () => ({
+  __esModule: true,
+  default: () => {
+    return <>Mock map page</>;
+  },
+}));
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+describe("tests for App", () => {
+  it("should render login page on initial route", () => {
+    render(
+      <MemoryRouter initialEntries={["/"]}>
+    <App />
+</MemoryRouter>
+     );
+    expect(screen.getByText("Mock Login Page")).toBeInTheDocument();
+  });
+
+  it("routes to the map page ", async () => {
+    render(
+      <MemoryRouter initialEntries={["/map"]}>
+    <App />
+</MemoryRouter>
+    )
+
 });
+})
