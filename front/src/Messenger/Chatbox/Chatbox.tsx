98d44ce42c932cce666a5204497c0152a493f0f7
makoalex <macovei.a2@gmail.com>
2023-12-22 12:17:36 +0100

    [feat] rendering the navbar

--- front/src/Messenger/Chatbox/Chatbox.tsx
+++ front/src/Messenger/Chatbox/Chatbox.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { ChatBoxInterface } from "../../Types";
+import NavbarChatBox from "./NavbarChatBox";
 
-export default function Chatbox({ socketId }: ChatBoxInterface) {
+export default function Chatbox({ socketId, userName }: ChatBoxInterface) {
   return (
     <section className="flex flex-col">
       <div className="w-[300px] h-[400px] flex flex-col bg-secondary">
-        Chatbox
+        <NavbarChatBox socketId={socketId} userName={userName}/>
       </div>
     </section>
   );
