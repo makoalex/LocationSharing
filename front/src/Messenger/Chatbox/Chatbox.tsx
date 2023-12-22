77fa0ac8f89ed871489cebd22b0e8c64cfc3fbea
makoalex <macovei.a2@gmail.com>
2023-12-22 11:53:00 +0100

    [feat] creating chatbox component

---
+++ front/src/Messenger/Chatbox/Chatbox.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { ChatBoxInterface } from "../../Types";
+import "../Messenger.css";
+
+export default function Chatbox({ socketId }: ChatBoxInterface) {
+  return (
+    <section className="flex flex-col">
+      <div className="w-[300px] h-[400px] flex flex-col bg-secondary">
+        Chatbox
+      </div>
+    </section>
+  );
+}
