54acf1dbcf4f59bac1d101a2fb2c478d89592128
makoalex <macovei.a2@gmail.com>
2023-12-23 20:28:06 +0100

    [feat] rendering the Messages and NewMessage component

--- front/src/Messenger/Chatbox/Chatbox.tsx
+++ front/src/Messenger/Chatbox/Chatbox.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { ChatBoxInterface } from "../../Types";
 import NavbarChatBox from "./NavbarChatBox";
+import Messages from "./Messages";
+import NewMessages from "./NewMessages";
 
 export default function Chatbox({ socketId, userName }: ChatBoxInterface) {
   return (
     <section className="flex flex-col">
-      <div className="w-[300px] h-[400px] flex flex-col bg-secondary border-black border-2 border-t-0 border-r-4">
+      <div className="w-[300px] h-[400px] flex flex-col justify-between bg-secondary border-black border-2 border-t-0 border-r-4">
         <NavbarChatBox socketId={socketId} userName={userName}/>
+        <Messages socketId={socketId} />
+        <NewMessages socketId={socketId} />
       </div>
     </section>
   );
