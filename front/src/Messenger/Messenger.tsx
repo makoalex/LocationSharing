60d676b7c22a23a74252630f136adaaac13cf36d
makoalex <macovei.a2@gmail.com>
2023-12-23 20:28:22 +0100

    [fix] fix style

--- front/src/Messenger/Messenger.tsx
+++ front/src/Messenger/Messenger.tsx
@@ -16,7 +16,7 @@ const dummy_chatBoxes = [
 ]
 export default function Messenger() {
   return (
-    <div className='h-[300px] absolute bottom-1 left-0 flex flex-row justify-center z-10 gap-5 ml-5 rounded-md'>
+    <div className='h-[300px] absolute bottom-1 left-0 flex flex-row z-10 gap-5 ml-5 rounded-md'>
       {dummy_chatBoxes.map((chatBox)=>(<Chatbox key={chatBox.socketId} socketId = {chatBox.socketId} userName={chatBox.userName}/>))}
     </div>
   )
