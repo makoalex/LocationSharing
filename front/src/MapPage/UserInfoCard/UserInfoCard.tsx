bf652fe1c31701e184da6caa7a30ddc20d03650f
makoalex <macovei.a2@gmail.com>
2023-12-29 17:56:52 +0100

    [chore] fixing the width and position of the user info card

--- front/src/MapPage/UserInfoCard/UserInfoCard.tsx
+++ front/src/MapPage/UserInfoCard/UserInfoCard.tsx
@@ -16,7 +16,7 @@ export default function UserInfoCard({
   );
   return (
    
-    <div className=" absolute bottom-2 left-5 flex flex-col w-[300px] h-[100px] border-2 border-black border-b-4 p-2 shadow-[4px_4px_0px_0px_#0B2447] bg-[#FCF8E7] transition-all duration-75">
+    <div className=" absolute top-2 left-5 flex flex-col w-[270px] h-[100px] border-2 border-black border-b-4 p-2 shadow-[4px_4px_0px_0px_#0B2447] bg-[#FCF8E7] transition-all duration-75">
       <Label text={username} className="text-lg font-primary" />
       <Label text={`${calculateDistanceBetweenCords({ coord1: myLocation, coord2: userLocation })} kms`} className={'text-base font-primary'} />
       <ActionButton socketId= {socketId} userName={username}/>
