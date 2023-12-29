f89f7a0b94b0d6bcb4cdd37e9b1ad913c291d269
makoalex <macovei.a2@gmail.com>
2023-12-29 17:59:52 +0100

    [feat] functionality for close chat button

--- front/src/Messenger/Chatbox/NavbarChatBox.tsx
+++ front/src/Messenger/Chatbox/NavbarChatBox.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { ChatBoxInterface } from "../../Types";
 import NavbarLabel from "./NavbarLabel";
-import closeIcon from "../../assets/close-icon.svg"
+import { useDispatch } from "react-redux";
+import { removeChatBox } from "../messengerSlice";
 
 
 const NavbarCloseButton = ({socketId}:ChatBoxInterface) => {
+  const dispatch = useDispatch();
+  const handleCloseChatBox= ()=>{
+    dispatch(removeChatBox(socketId))
+  }
     return(
         <div className="">
-      <button className="text-lg rounded-sm w-[25px] h-[25px]  bg-callAction active:bg-accent cursor-pointer  p-2 font-primary font-extrabold flex flex-col justify-center items-center shadow-[2px_2px_0px_0px_#0B2447] border-2 border-black focus:bg-accent focus:transition-all focus:duration-300 focus:shadow-none  ">X</button>
+      <button onClick={handleCloseChatBox} className="text-lg rounded-sm w-[25px] h-[25px]  bg-callAction active:bg-accent cursor-pointer  p-2 font-primary font-extrabold flex flex-col justify-center items-center shadow-[2px_2px_0px_0px_#0B2447] border-2 border-black focus:bg-accent focus:transition-all focus:duration-300 focus:shadow-none  ">X</button>
       </div>
     )
 
