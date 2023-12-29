2647133c43efa9486e7a787ebc5febdf01d6111a
makoalex <macovei.a2@gmail.com>
2023-12-29 17:58:10 +0100

    [feat] adding real users, removing dummy ones

--- front/src/Messenger/Messenger.tsx
+++ front/src/Messenger/Messenger.tsx
@@ -1,23 +1,22 @@
-import React from 'react'
-import Chatbox from './Chatbox/Chatbox'
+import React from "react";
+import Chatbox from "./Chatbox/Chatbox";
+import { useSelector } from "react-redux";
+import { chatState } from "../Types";
 
 
-const dummy_chatBoxes = [
-    {
-        userName: "John",
-        socketId:'321121',
-        messages:[]
-    },
-    {
-        userName: "Mako",
-        socketId:'675435',
-        messages:[]
-    }
-]
 export default function Messenger() {
+  const chatBoxes = useSelector(
+    (state: { messenger: chatState }) => state.messenger.chatBoxes
+  );
   return (
-    <div className='h-[300px] absolute bottom-1 left-0 flex flex-row z-10 gap-5 ml-5 rounded-md'>
-      {dummy_chatBoxes.map((chatBox)=>(<Chatbox key={chatBox.socketId} socketId = {chatBox.socketId} userName={chatBox.userName}/>))}
+    <div className="h-[300px] absolute bottom-1 left-0 flex flex-row z-10 gap-5 ml-5 rounded-md">
+      {chatBoxes.map((chatBox) => (
+        <Chatbox
+          key={chatBox.socketId}
+          socketId={chatBox.socketId}
+          userName={chatBox.userName}
+        />
+      ))}
     </div>
-  )
+  );
 }
