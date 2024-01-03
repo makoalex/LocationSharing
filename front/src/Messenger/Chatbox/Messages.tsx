55e1d0855ff0b8637cb146c64ae96c5899c50587
makoalex <macovei.a2@gmail.com>
2024-01-03 17:33:16 +0100

    [feat] rendering real messages

--- front/src/Messenger/Chatbox/Messages.tsx
+++ front/src/Messenger/Chatbox/Messages.tsx
@@ -1,25 +1,16 @@
 import React from "react";
 import SingleMessage from "./SingleMessage";
-import {ISingleMessage} from "../../Types";
+import {ISingleMessage, chatState} from "../../Types";
 import { ChatBoxInterface } from "../../Types";
+import { useSelector } from "react-redux";
 
 
-const messages:ISingleMessage[] = [
-  {
-    id:1,
-    myMessage: true,
-    content:'romeo?'
-  },
-  {
-    id:2,
-    myMessage: false,
-    content:" what's in a name for which we call a rose by any other name would smell as sweet"
-  }
-]
+
 export default function Messages({ socketId }: ChatBoxInterface) {
+  const messages = useSelector((state:{messenger:chatState}) => state.messenger.chatHistory[socketId!] ) 
   return (
     <section className="flex flex-grow flex-col overflow-y-auto w-full font-secondary text-sm tracking-wider font-light">
-      {messages.map((message)=>{
+      {messages?.map((message)=>{
         return <SingleMessage key={message.id} myMessage={message.myMessage} content={message.content}/>})
       }
 
