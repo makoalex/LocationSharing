6baa810457712f77ff6a94978e51b620867e0580
makoalex <macovei.a2@gmail.com>
2024-01-30 21:49:06 +0100

    [feat] creating join room button

--- front/src/VideoRooms/VideoRoom.tsx
+++ front/src/VideoRooms/VideoRoom.tsx
@@ -1,26 +1,67 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import store from '../store/store'
-import CreateRoomButton from './CreateRoomButton';
+import React from "react";
+import { useSelector } from "react-redux";
+import store from "../store/store";
+import CreateRoomButton from "./CreateRoomButton";
+import RoomJoinedButton from "./RoomJoinedButton";
 
-interface RoomState{
+interface RoomState {
   inRoom: string;
   rooms: string[];
 }
-
-export const RoomList= ()=>{
-    // const rooms = useSelector((state:{room:RoomState})=>state.room.rooms)
-    return(
-        <div>
-          <CreateRoomButton/>
-        </div>
-    )
+interface IParticipants {
+  socketId: string;
+  peerId: number;
+  username: string;
 }
+interface IRoom {
+  id: number;
+  participants: IParticipants[];
+}
+
+const DummyRooms: IRoom[] = [
+  {
+    id: 1,
+    participants: [
+      {
+        socketId: "1",
+        peerId: 1,
+        username: "Mako",
+      },
+    ],
+  },
+  {
+    id: 2,
+    participants: [
+      {
+        socketId: "2",
+        peerId: 2,
+        username: "John",
+      },
+    ],
+  },
+];
+
+export const RoomList = () => {
+  // const rooms = useSelector((state:{room:RoomState})=>state.room.rooms)
+  return (
+    <div className="flex float-col justify-between w-screen h-[60px]">
+      <CreateRoomButton />
+      {DummyRooms.map((room) => (
+        <RoomJoinedButton
+          key={room.id}
+          creatorUsername={room.participants[0].username}
+          roomId={room.id}
+          amountOfParticipants={room.participants.length}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default function VideoRoom() {
   return (
     <div>
       <RoomList />
     </div>
-  )
+  );
 }
