ed974a6e10167b96a3b35d68ea43e5f5a6f227ce
makoalex <macovei.a2@gmail.com>
2024-01-30 21:48:35 +0100

    [fix] update import

--- front/src/VideoRooms/CreateRoomButton.tsx
+++ front/src/VideoRooms/CreateRoomButton.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Call from '../assets/Call.png'
+import Call from '../assets/Group.png'
 
 export default function CreateRoomButton() {
   return (
     <div >
-        <img src={Call} alt=" call button" className='absolute right-14 bottom-4 rounded-sm  w-[60px] h-[50px]  bg-black  active:shadow-none active:transition-all active:duration-300 cursor-pointer' />
+        <img src={Call} alt=" call button" className='absolute right-16 bottom-12 rounded-sm  w-[50px] h-[40px] shadow-[5px_5px_0px_0px_#000]    active:shadow-none active:bg-none active:transition-all active:duration-300 cursor-pointer' />
     </div>
   )
 }
