4db74a88691280cfab9bfd04a069bfe102c9f1b3
makoalex <macovei.a2@gmail.com>
2024-03-05 21:41:33 +0100

    [fix] style landing page

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -11,19 +11,19 @@ module.exports = {
         DEFAULT: "10px",
       },
       boxShadow: {
-        xl: "7px 7px 0px -0px #0B2447",
+        xl: "7px 7px 0px 0px #0B2447",
       },
     },
     screens: {
       xs: "320px",
       sm: "640px",
       md: "768px",
-      lg: "960px",
+      lg: "950px",
       xl: "1200px",
     },
     extend: {
       colors: {
-        primary: "#E9DDFF",
+        primary: "#FBF3F0ff",
         callAction: "#CEED40",
         disabledButton:"#a388ee",
         secondary: "#F5F5F5ff",
