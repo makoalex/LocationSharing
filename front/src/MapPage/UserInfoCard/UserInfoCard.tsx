21b235d42492ef97a0363689d5a86214635a50b0
makoalex <macovei.a2@gmail.com>
2024-03-05 22:37:38 +0100

    [chore] style changes

--- front/src/MapPage/UserInfoCard/UserInfoCard.tsx
+++ front/src/MapPage/UserInfoCard/UserInfoCard.tsx
@@ -16,9 +16,9 @@ export default function UserInfoCard({
   );
   return (
    
-    <div className=" absolute top-2 left-5 flex flex-col w-[270px] h-[100px] border-2 border-black border-b-4 p-2 shadow-[4px_4px_0px_0px_#0B2447] bg-[#FCF8E7] transition-all duration-75">
+    <div className=" absolute top-2 left-5 flex flex-col w-[200px] h-[100px] border-2 border-black border-b-4 p-2 shadow-[4px_4px_0px_0px_#0B2447] bg-primary transition-all duration-75">
       <Label text={username} className="text-lg font-primary" />
-      <Label text={`${calculateDistanceBetweenCords({ coord1: myLocation, coord2: userLocation })} kms`} className={'text-base font-primary'} />
+      <Label text={`${calculateDistanceBetweenCords({ coord1: myLocation, coord2: userLocation })} kms`} className={'text-base font-primary mt-5'} />
       <ActionButton socketId= {socketId} username={username}/>
     </div>
 
--- front/src/Messenger/Chatbox/Chatbox.tsx
+++ front/src/Messenger/Chatbox/Chatbox.tsx
@@ -7,8 +7,8 @@ import NewMessages from "./NewMessages";
 export default function Chatbox({ socketId, username }: ChatBoxInterface) {
   return (
     <section className="flex flex-col">
-      <div className="w-[270px] h-[300px] flex flex-col justify-between bg-secondary border-black border-2  border-r-4  ">
-        <NavbarChatBox socketId={socketId} username={username}/>
+      <div className="w-[270px] h-[300px] flex flex-col justify-between bg-primary border-black border-2  border-r-4  ">
+        <NavbarChatBox socketId={socketId} username={username} />
         <Messages socketId={socketId} />
         <NewMessages socketId={socketId} />
       </div>
